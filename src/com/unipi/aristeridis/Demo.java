87622c4d8e3d9cda7997ede345f1a485353f7aa1
Alexandros <aristeridis@hotmail.gr>
2022-12-04 01:15:30 +0200

    2nd

---
+++ src/com/unipi/aristeridis/Demo.java
@@ -0,0 +1,10 @@
+package com.unipi.aristeridis;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+    static void giveMeFuncionality(SomeObject so){
+
+    }
+}
---
+++ src/com/unipi/aristeridis/SomeObject.java
@@ -0,0 +1,4 @@
+package com.unipi.aristeridis;
+
+public class SomeObject {
+}
